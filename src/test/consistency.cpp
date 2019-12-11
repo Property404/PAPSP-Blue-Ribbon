3731937a1788a7da36bee67646c78f5ffc9fc00c
Dagan Martinez <dagan.martinez@abaco.com>
2019-12-10 19:54:35 -0600

    Add unit test

---
+++ src/test/consistency.cpp
@@ -0,0 +1,65 @@
+#include "catch.hpp"
+#include "Graph.hxx"
+#include "serial.hxx"
+#include "parallel.hxx"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+static void check(Graph& graph, int size, int threads, int subthreads) 
+{
+	vector<vector<int>> serial_dist_matrix(size, vector<int>(size));
+	vector<vector<int>> serial_prev_matrix(size, vector<int>(size));
+	vector<vector<int>> parallel_dist_matrix(size, vector<int>(size));
+	vector<vector<int>> parallel_prev_matrix(size, vector<int>(size));
+
+	serialAPSP(graph, serial_dist_matrix, serial_prev_matrix);
+	parallelAPSP(
+			graph,
+			parallel_dist_matrix,
+			parallel_prev_matrix,
+			threads,
+			subthreads
+	);
+
+	for(int i=0;i<size;i++)
+	{
+		REQUIRE(serial_dist_matrix[i] == parallel_dist_matrix[i]);
+	}
+}
+
+#define MIN_SIZE 2
+#define LOWER_MAX_SIZE 10
+#define HIGHER_MAX_SIZE 100
+
+TEST_CASE( "Serial and parallel results are consistent with each other", "[serial parallel]" )
+{
+
+	for(int size=MIN_SIZE;size<=LOWER_MAX_SIZE;size++)
+	{
+		Graph graph(size);
+		for(int threads=1;threads<size*4;threads++)
+		{
+
+			if(threads <= size)
+				for(int subthreads=-1;subthreads<size;subthreads++)
+				{
+					if(subthreads == 0)
+						continue;
+					check(graph, size, threads, subthreads);
+				}
+			else
+				check(graph, size, threads, -1);
+		}
+	}
+}
+
+TEST_CASE( "Serial and parallel results are consistent for large values", "[serial parallel]" )
+{
+
+	for(int size=MIN_SIZE;size<=HIGHER_MAX_SIZE;size++)
+	{
+		Graph graph(size);
+		check(graph, size, 1+rand()%size, -1);
+	}
+}
