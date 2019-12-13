705dd8df552a51044b51e1e07940bca1c5f7ff91
Class Account - UAH <uahcls23@dmcvlogin4.asc.edu>
2019-12-12 18:52:03 -0600

    Add shit times

--- graph_forge/forge.sh
+++ graph_forge/forge.sh
@@ -1,16 +1,19 @@
 OUTPUT=data.js
 FIRST_RUN=../first_run/
+DOUBLE_SUNNIES=../double_subthreads/
 
 SERIAL_FILE=${FIRST_RUN}/serial_out
 PARALLEL_FILE=${FIRST_RUN}/parallel_out
+SHIT_FILE=${DOUBLE_SUNNIES}/parallel_out
 
 mkdir -p parallel_parsed
+mkdir -p shit_parallel_parsed
 
 echo "const SERIAL_TIMES_BY_SIZE = {" > $OUTPUT
 
 cat ${FIRST_RUN}/serial_out | grep -v Size | grep -v DONE | awk '{ print "\""$1"\":"$2"," }' >> $OUTPUT
 
-echo "}" >> $OUTPUT
+echo "};" >> $OUTPUT
 
 echo "const PARALLEL_TIMES_BY_SIZE = {" >> $OUTPUT
 
@@ -23,4 +26,17 @@ done
 pushd parallel_parsed
 paste sizes 1 2 4 8 16 | awk '{ print "\""$1"\":["$2","$3","$4","$5", "$6"],"}' >> ../$OUTPUT
 popd
-echo "}" >> $OUTPUT;
+echo "};" >> $OUTPUT;
+
+echo "const SHIT_TIMES_BY_SIZE = {" >> $OUTPUT
+
+cat ${SHIT_FILE} | grep -v DONE | grep -v Size | awk '{print $2" "$1" "$3 }' | grep "^1 " --color=no | awk '{print $2}' > shit_parallel_parsed/sizes
+
+for i in  1 2 4 8 16
+do
+cat ${SHIT_FILE} | grep -v DONE | grep -v Size | awk '{print $2" "$1" "$3 }' | grep "^$i " --color=no | awk '{print $3}' > shit_parallel_parsed/$i
+done
+pushd shit_parallel_parsed
+paste sizes 1 2 4 8 16 | awk '{ print "\""$1"\":["$2","$3","$4","$5", "$6"],"}' >> ../$OUTPUT
+popd
+echo "};" >> $OUTPUT;
